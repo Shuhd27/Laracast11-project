7898ffc729d8c011691df897d35c5b24a3367979
Shuhd27 <shuhdalazzani27@gmail.com>
2024-10-14 13:25:36 +0200

    les 10 af

--- database/migrations/2024_09_23_132428_create_job_listings_table.php
+++ database/migrations/2024_09_23_132428_create_job_listings_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('employer_id');
+          //  $table->foreignIdFor(\App\Models\Employer::class);
             $table->string('title');
-            $table->string('salary' );
+            $table->string('salary');
             $table->timestamps();
         });
     }
