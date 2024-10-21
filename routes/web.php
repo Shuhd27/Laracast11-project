d5f46fd049bb92917488c76c644281856cd0ad1b
Shuhd27 <shuhdalazzani27@gmail.com>
2024-10-21 15:24:14 +0200

    les 18 af

--- routes/web.php
+++ routes/web.php
@@ -53,14 +53,55 @@ Route::get('/jobs/{id}/edit', function ($id) {
     return view('jobs.edit', ['job' => $job]);
 });
 
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // authorize (on hold....)
+    // update the job
+    $job = Job::findOrFail($id); // null
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+    // Andere manier om de job te updaten
+/*
+    $job->title = request('title');
+    $job->salary = request('salary');
+    $job->save();
+*/
+    // redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destory
+Route::delete('/jobs/{id}', function ($id) {
+    // Authorize (on hold....)
+    
+    // Delete
+    Job::findOrFail($id)->delete();
+
+    // Redirect
+    return redirect('/jobs');
+
+});
 
 Route::get('/contact', function () {
     return view('contact');
 });
 
+/*
+// een ander manier om de job te verwijderen maar de manier die ik heb gebruikt is wat makkelijker!
+// ik schrijf het hier gwn zodat ik het niet vergeet that's it.
+$job = Job::findOrFail($id);
+    $job->delete()
 
-
-
+*/
 
 
 /*
