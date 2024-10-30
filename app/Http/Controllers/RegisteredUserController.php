353d6d603f2f071db011fcc0dc95b8bfd8d71b59
Shuhd27 <shuhdalazzani27@gmail.com>
2024-10-30 13:01:00 +0100

    les 22 af

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +17,22 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // validate
+
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Password::min(6)],
+        ]);
+
+        // create the user
+        $user = User::create($attributes);
+
+        // log in
+        Auth::login($user);
+
+        // redirect
+        return redirect('/jobs');
     }
 }
